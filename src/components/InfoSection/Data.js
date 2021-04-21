23b38908388e679b3974c5362bbb660b985b4c3a
lamboyjat <lamboyjat@gmail.com>
2021-04-21 02:05:44 +0200

    component of Hero section, Images is not showing to be fixed

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,17 @@
+export const homeObject = {
+  id: "about",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Premium Knowledge",
+  headLine: "Unlimited Resources with zero fees",
+  description:
+    "Get access to our exclusive app that allow you to share unlimited ideas without getting to pay any fees.",
+  buttonLabel: "Get Started",
+  imgStart: false,
+  img: require("../../images/svg-1.png"),
+  alt: "Car",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
